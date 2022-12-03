18bb24b05158e681269632b29c221376814b1670
1rara <17914+Jason-Li@users.noreply.github.tamu.edu>
2022-12-02 18:31:47 -0600

    split render module

--- main.py
+++ main.py
@@ -8,6 +8,8 @@ from stable_baselines3.common.env_checker import check_env
 import RoboticArmEnv_2Robots_Incremental as RAE
 import time
 
+import render
+
 #number of arm segments
 N_ARMS=2
 register(
@@ -18,32 +20,32 @@ register(
 )
 
 # Parallel environments
-env = make_vec_env("RoboticArmEnv-v1", n_envs=1)
+env = make_vec_env("RoboticArmEnv-v1", n_envs=8)
 
 # Single Threaded Env
 # env = RAE.RoboticArmEnv_V1(training=True, num_arms=N_ARMS)
 
 # It will check your custom environment and output additional warnings if needed
 # check_env(env)
-#
 # model = A2C("MlpPolicy", env, verbose=2)
 # model.learn(total_timesteps=100000)
 # model.save("a2c")
 
-model = PPO("MlpPolicy", env, verbose=3)
-model.learn(total_timesteps=1000000)
-model.save("ppo")
-#
-# model = DQN("MlpPolicy", env, verbose=3, exploration_fraction=0.70)
+# model = PPO("MlpPolicy", env, verbose=2)
+# model.learn(total_timesteps=1000000)
+# model.save("ppo")
+
+# model = DQN("MlpPolicy", env, verbose=2, exploration_fraction=0.70)
 # model.learn(total_timesteps=1000000)
 # model.save("dqn")
-# #
+
 # del model # remove to demonstrate saving and loading
 
 # model = A2C.load("a2c")
 model = PPO.load("ppo")
 # model = DQN.load("dqn")
 
+render.render_init()
 episodes = 100
 env = RAE.RoboticArmEnv_V1(training=False, num_arms=N_ARMS)
 for episode in range(episodes):
@@ -61,7 +63,7 @@ for episode in range(episodes):
 
         # print('obs',obs,'reward', reward)
         cumReward+=reward
-        env.render()
+        render.render(env)
         time.sleep(0.05) #slow down the animation
     print('Reward:', cumReward)
 
---
+++ render.py
@@ -0,0 +1,153 @@
+import numpy as np
+import pygame
+from pygame.locals import *
+import glm
+from OpenGL.GL import *
+from OpenGL.GLU import *
+
+def render_init():
+    pygame.init()
+    display = (800, 600)
+    pygame.display.set_mode(display, DOUBLEBUF | OPENGL)
+    gluPerspective(45, (display[0] / display[1]), 0.01, 100.0)
+    glTranslatef(0.0, -10.0, -45)
+
+
+def render(env):
+    # Uncomment to rotate the scene
+    # glRotatef(-1.0, 0, 1, 0)
+
+    # RENDER Robotic Arm
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            pygame.quit()
+            quit()
+
+    glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT)
+    glColor((1, 1, 1))
+    cube_centers = []
+    end_effectors = []
+
+    for r in range(env.num_robots):
+        cubes = []
+        m = glm.mul(glm.rotate(-glm.pi() / 2, (0, 0, 1)), glm.translate(env.robot_roots[r]))
+        m = glm.mul(m, glm.rotate(env.theta[0 + env.num_arms * r], (0, 1, 0)))
+        RenderCube(env, m)
+        cubes.append(m * glm.vec4(0, 0, 0, 1))
+        m = glm.mul(m, glm.rotate(env.phi[0 + env.num_arms * r], (0, 0, 1)))
+        RenderArm(env, m)
+
+        for i in range(1, env.num_arms):
+            m = glm.mul(glm.mul(glm.mul(m, glm.translate((env.arm_length, 0, 0,))),
+                                glm.rotate(env.theta[i + env.num_arms * r], (1, 0, 0))),
+                        glm.rotate(-glm.pi(), (0, 0, 1)))
+            RenderCube(env, m)
+            cubes.append(m * glm.vec4(0, 0, 0, 1))
+            m = glm.mul(m, glm.rotate(env.phi[i + env.num_arms * r], (0, 0, 1)))
+            RenderArm(env, m)
+
+        m = glm.mul(glm.mul(glm.mul(m, glm.translate((env.arm_length, 0, 0,))), glm.rotate(0, (1, 0, 0))),
+                    glm.rotate(-glm.pi(), (0, 0, 1)))
+        RenderCube(env, m)
+        cubes.append(m * glm.vec4(0, 0, 0, 1))
+        cube_centers.append(cubes)
+        end_effectors.append(glm.vec3(cubes[-1]))
+
+    # RENDER DESTINATION
+    glColor((1, 1, 0))
+    for d in range(env.num_robots):
+        RenderSphere(env, (env.dest[d:d+3], env.destSize))
+
+    for d in range(env.num_robots):
+        RenderLine(env, end_effectors[d], env.dest[d:d+3])
+
+    # RENDER OBSTACLES
+    # for obstacle in env.obstacles:
+    #     glColor((1, 0.2, 0.2))
+    #     env.RenderSphere(obstacle)
+
+    pygame.display.flip()
+
+def RenderArm(env, transform):
+        box_edges = [
+            (0, 1),
+            (0, 3),
+            (0, 4),
+            (2, 1),
+            (2, 3),
+            (2, 6),
+            (7, 3),
+            (7, 4),
+            (7, 6),
+            (5, 1),
+            (5, 4),
+            (5, 6)
+        ]
+        arm_vertices = [
+            glm.vec4(env.arm_length, -env.arm_width, -env.arm_width, 1),
+            glm.vec4(env.arm_length, env.arm_width, -env.arm_width, 1),
+            glm.vec4(0, env.arm_width, -env.arm_width, 1),
+            glm.vec4(0, -env.arm_width, -env.arm_width, 1),
+            glm.vec4(env.arm_length, -env.arm_width, env.arm_width, 1),
+            glm.vec4(env.arm_length, env.arm_width, env.arm_width, 1),
+            glm.vec4(0, env.arm_width, env.arm_width, 1),
+            glm.vec4(0, -env.arm_width, env.arm_width, 1)
+        ]
+        glBegin(GL_LINES)
+        for edge in box_edges:
+            for vertex in edge:
+                v = glm.mul(transform, arm_vertices[vertex])
+                v_ = (v.x, v.y, v.z)
+                glVertex3fv(v_)
+        glEnd()
+
+def RenderCube(env, transform):
+    box_edges = [
+        (0, 1),
+        (0, 3),
+        (0, 4),
+        (2, 1),
+        (2, 3),
+        (2, 6),
+        (7, 3),
+        (7, 4),
+        (7, 6),
+        (5, 1),
+        (5, 4),
+        (5, 6)
+    ]
+    box_vertices = [
+        glm.vec4(2 * env.arm_width, -2 * env.arm_width, -2 * env.arm_width, 1),
+        glm.vec4(2 * env.arm_width, 2 * env.arm_width, -2 * env.arm_width, 1),
+        glm.vec4(-2 * env.arm_width, 2 * env.arm_width, -2 * env.arm_width, 1),
+        glm.vec4(-2 * env.arm_width, -2 * env.arm_width, -2 * env.arm_width, 1),
+        glm.vec4(2 * env.arm_width, -2 * env.arm_width, 2 * env.arm_width, 1),
+        glm.vec4(2 * env.arm_width, 2 * env.arm_width, 2 * env.arm_width, 1),
+        glm.vec4(-2 * env.arm_width, 2 * env.arm_width, 2 * env.arm_width, 1),
+        glm.vec4(-2 * env.arm_width, -2 * env.arm_width, 2 * env.arm_width, 1)
+    ]
+    glBegin(GL_LINES)
+    for edge in box_edges:
+        for vertex in edge:
+            v = glm.mul(transform, box_vertices[vertex])
+            v_ = (v.x, v.y, v.z)
+            glVertex3fv(v_)
+    glEnd()
+
+def RenderLine(env, a0, a1):
+    a0 = np.array(a0)
+    a1 = np.array(a1)
+    dist = np.sqrt(np.sum(np.square(a1 - a0))) / env.arm_length / env.num_arms / 2
+    glColor((dist, 1-dist, 0))
+    glBegin(GL_LINES)
+    glVertex3fv(a0)
+    glVertex3fv(a1)
+    glEnd()
+
+def RenderSphere(env, obstacle):
+    glPushMatrix()
+    glTranslatef(obstacle[0][0], obstacle[0][1], obstacle[0][2])  # Move to the place
+    glScale(obstacle[1], obstacle[1], obstacle[1])
+    sphere_base = gluNewQuadric()
+    gluSphere(sphere_base, 1.0, 32, 16)  # Draw sphere
+    glPopMatrix()
