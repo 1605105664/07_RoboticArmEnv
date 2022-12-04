ce373f7d19376d17dcd10b621dae9f5b0b12ad3f
Dillon Fisher <Dillon.Fisher.14@tamu.edu>
2022-12-04 15:18:18 -0600

    Moved destination for two robots

--- train.py
+++ train.py
@@ -13,14 +13,10 @@ import random
 
 # initialize CLI
 parser = argparse.ArgumentParser(description='CLI for train module')
-parser.add_argument('-nr', '--N_ROBOTS', help='# of robot', default=1, type=int)
+parser.add_argument('-nr', '--N_ROBOTS', help='# of robot', default=2, type=int)
 parser.add_argument('-na', '--N_ARMS', help='# of arms per robot', default=2, type=int)
 parser.add_argument('-a', '--ALPHA', help='alpha value', default=0.5, type=float)
-parser.add_argument('-d', '--DESTSIZE', help='destination size', default=5, type=int)
 parser.add_argument('-m', '--MAX_STEP', help='maximum timesteps', default=1000, type=int)
-parser.add_argument('-e', '--TIMESTEPS', help='total timesteps to train', default=1e5, type=int)
-parser.add_argument('-i', '--input', help='input model path', default=None, type=str)
-parser.add_argument('-o', '--output', help='output model path', default=None, type=str)
 args = vars(parser.parse_args())
 
 # number of arm segments
@@ -28,23 +24,18 @@ register(
     id="RoboticArmEnv-v1",
     entry_point=RAE.RoboticArmEnv_V1,
     max_episode_steps=args['MAX_STEP'],
-    kwargs={'num_arms': args['N_ARMS'], 'alpha_reward': args['ALPHA'],
-            'num_robots': args['N_ROBOTS'], 'destSize': args['DESTSIZE']
-            }
+    kwargs={'num_arms': args['N_ARMS'], 'alpha_reward': args['ALPHA'], 'num_robots': args['N_ROBOTS']}
 )
 
 if __name__ == '__main__':
     # Get current timestamp as JobID
     JobID = str(round(time.time()))+str(random.randrange(1e5, 1e6))  # include random seed to prevent collision
-    fname = 'output/ppo/'+'_'.join(map(str, list(args.values())[:4]))+'/'+JobID
-    if args['output']:
-        fname = args['output']
     print('args:', args)
     print('JobID:', JobID)
 
     # Parallel environments
     env = SubprocVecEnv([lambda: gym.make("RoboticArmEnv-v1")]*4)
-    env = VecMonitor(env, fname)
+    env = VecMonitor(env, './output/ppo/'+JobID)
 
     # Single Threaded Env
     # env = RAE.RoboticArmEnv_V1(training=True, num_arms=args['N_ARMS'], alpha_reward=args['ALPHA'], num_robots=args['N_ROBOTS'])
@@ -52,9 +43,16 @@ if __name__ == '__main__':
 
     # It will check your custom environment and output additional warnings if needed
     # check_env(env)
+    # model = A2C("MlpPolicy", env, verbose=2)
+    # model.learn(total_timesteps=100000)
+    # model.save("a2c")
 
     model = PPO("MlpPolicy", env, verbose=2)
-    if args['input']:
-        model = PPO.load(args['input'], env)
-    model.learn(total_timesteps=args['TIMESTEPS'])
-    model.save(fname)
+    model.learn(total_timesteps=1e7)
+    model.save("./output/ppo/"+JobID)
+
+    # model = DQN("MlpPolicy", env, verbose=2, exploration_fraction=0.70)
+    # model.learn(total_timesteps=100)
+    # model.save("dqn")
+
+    # del model # remove to demonstrate saving and loading
